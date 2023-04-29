b352d7e138fd3415cd6952fa50a1924c2bb1416d
Celix <alonsomurua@gmail.com>
2023-04-29 00:17:45 -0400

    sombras a elementos

--- JS/obtener_ropa.js
+++ JS/obtener_ropa.js
@@ -5,12 +5,16 @@ $(document).ready(function(){
             $.each(data, function(i, item) {
                 
                 var elemento = `
-                    <a class="col-sm-12 col-md-4 col-lg-3 nav-link" href="royalCanin.html">
-                        <img src="${item.image}" class="img-fluid" style="height: 300px;" alt="Imagen producto">
-                        <p> ${item.price} </p>
-                        <p> ${item.title} </p>
-                        <p> ${item.description} </p>
-                    </a>
+                    <div class="col-sm-12 col-md-4 col-lg-3">
+                        <a style="height: 650px;" class="overflow-hidden p-3 shadow nav-link" href="royalCanin.html">
+                            <div>
+                                <img src="${item.image}" class="img-fluid" style="height: 200px;" alt="Imagen producto">
+                                <p class="p-3"> $${item.price} </p>
+                                <p class="h5 fw-bold"> ${item.title} </p>
+                            </div>
+                            <p class="p-3"> ${item.description} </p>
+                        </a>
+                    </div>
                 `;
 
                 $('#grid').append(elemento);
