319903b5af27c239815d8607626ca5092770387a
TaintedNobody <carl.uribe@duocuc.cl>
2023-05-05 20:02:20 -0400

    agregue las validaciones

--- JS/valida_registro.js
+++ JS/valida_registro.js
@@ -54,5 +54,60 @@ $(document).ready(function() {
             }
         }
     });
+});
 
+
+$(document).ready(function() {
+    $('#formulario2').validate({
+        rules: {
+            InputCorreo:{
+                required: true,
+                email: true
+            },
+            InputClave:{
+                required: true,
+                minlength: 5
+            }
+        },
+        messages:{
+            InputCorreo:{
+                required: "El correo es un campo obligatorio",
+                email: "El correo no cumple con el formato de un correo"
+            },
+            InputClave:{
+                required: "La contraseña es un campo obligatorio",
+                minlength: "La longitud mínima es de 5 caracteres"
+            }
+        }
+    });
+
+});
+
+$(document).ready(function(){
+    $('#formulario3').validate({
+        rules: {
+            InputId:{
+                required: true,
+                email: false
+            },
+            InputPrecio:{
+                required: true,
+                email: false
+            },
+            select:{
+                required: true
+            }
+        },
+        messages: {
+            InputId:{
+                required: "La id es un campo obligatorio"
+            },
+            InputPrecio:{
+                required: "El precio es un campo obligatorio"
+            },
+            select:{
+                required: "Debe seleccionar una categoria"
+            }
+        }
+    });
 });
