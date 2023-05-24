582ded85f8a503d142e790d8555e109fbe4564e4
TaintedNobody <carl.uribe@duocuc.cl>
2023-05-23 21:39:51 -0400

    arregle los type de las cajas de texto y puse el rest

--- JS/valida_mantenedor_prod.js
+++ JS/valida_mantenedor_prod.js
@@ -3,12 +3,20 @@ $(document).ready(function() {
         rules: {
             InputCant:{
                 required: true
+            },
+            InputDscOferta:{
+                required: true,
+                min: 1
             }
         },
         messages:{
             InputCant:{
                 required: "La cantidad es un campo obligatorio",
                 
+            },
+            InputDscOferta:{
+                required: "La cantidad es un campo obligatorio",
+                min: "El minimo es mayor que 1"
             }
         }
     });
